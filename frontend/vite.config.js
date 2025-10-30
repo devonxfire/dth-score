83fd0318807e88705d5b06f1323781702d5008a9
devonmartindale <devonmartindale@gmail.com>
2025-10-30 07:08:41 +0200

    Vite config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,18 @@
 process.env.LIGHTNING_CSS_FORCE_WASM = process.env.LIGHTNING_CSS_FORCE_WASM || '1';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5050',
+export default async () => {
+  // Dynamically import tailwind plugin after forcing wasm fallback
+  const tailwindcss = (await import('@tailwindcss/vite')).default;
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': 'http://localhost:5050',
+      },
+      allowedHosts: ['f91a54fd4849.ngrok-free.app'],
     },
-    allowedHosts: ['f91a54fd4849.ngrok-free.app'],
-  },
-})
+  })
+}
